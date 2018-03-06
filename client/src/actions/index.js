ada8f1ffeb34683f447120685450e4b24a858c79
Markus <markus.maelzer@gmail.com>
2018-03-06 23:30:17 +0100

    changed ROOT_URL ONCE AND FOR ALL

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,4 +2,4 @@ export * from './user-actions';
 export * from './admin-actions';
 export * from './auth-actions';
 
-export const ROOT_URL = 'http://a1013e60.ngrok.io';
+export const ROOT_URL = 'https://digi-pass.herokuapp.com';
